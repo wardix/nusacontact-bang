ce57ca906419d2d21c085cf9981a6b4030b747e6
Wardi <wardi@nusa.net.id>
2024-06-21 21:39:50 +0700

    feat: add basic authentication and logging to the application
    - Updated .env.dist to include BASIC_USERS for storing basic authentication users
    - Imported and implemented basicAuth middleware for the /bang route
    - Imported and applied logger middleware to log requests

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,19 @@
 import { Hono } from 'hono'
+import { logger } from 'hono/logger'
+import { basicAuth } from 'hono/basic-auth'
 import { processBangAiRequest } from './ai'
 
 const port = process.env.PORT || 3000
 const app = new Hono()
+
+app.use(logger())
+
+app.use('/bang', basicAuth({
+  verifyUser: (username, password, _c) => {
+    const validUser = JSON.parse(process.env.BASIC_USERS!)
+    return validUser.includes(`${username}:${password}`)
+  }
+}))
 
 app.get('/', (context) => {
   return context.text('Hello Hono!')
