42eb1c458df95d58844e38c24a42437ce3ef3fae
Wardi <wardi@nusa.net.id>
2024-10-29 09:40:38 +0700

    feat: add axios for message sending and improve error handling
    - Added axios as a dependency to handle HTTP requests.
    - Updated sendMessageReply function to use axios for API calls.
    - Improved error handling with clear messages for missing environment variables and non-200 responses.
    - Enhanced readability by restructuring the function and using try-catch for error logging.

--- src/nusacontact.ts
+++ src/nusacontact.ts
@@ -1,39 +1,53 @@
+import axios from 'axios'
+
 export async function sendMessageReply(
   to: string,
   message: string,
   id: string,
   via: string,
 ) {
-  const nusacontactPhoneNumberIds = JSON.parse(
-    process.env.NUSACONTACT_PHONE_NUMBER_IDS!,
-  )
-  const isProduction = nusacontactPhoneNumberIds.includes(via)
-  const apiUrl = isProduction
-    ? process.env.NUSACONTACT_MESSAGES_API_URL
-    : process.env.NUSACONTACT_MESSAGES_API_TEST_URL
-  const apiKey = isProduction
-    ? process.env.NUSACONTACT_API_KEY
-    : process.env.NUSACONTACT_API_TEST_KEY
+  try {
+    const nusacontactPhoneNumberIds = JSON.parse(
+      process.env.NUSACONTACT_PHONE_NUMBER_IDS || '[]',
+    )
+    const isProduction = nusacontactPhoneNumberIds.includes(via)
+    const apiUrl = isProduction
+      ? process.env.NUSACONTACT_MESSAGES_API_URL
+      : process.env.NUSACONTACT_MESSAGES_API_TEST_URL
+    const apiKey = isProduction
+      ? process.env.NUSACONTACT_API_KEY
+      : process.env.NUSACONTACT_API_TEST_KEY
 
-  const url = `${apiUrl}?phone_number_id=${via}`
+    if (!apiUrl || !apiKey) {
+      throw new Error('Missing API URL or API Key in environment variables.')
+    }
 
-  const data = {
-    messaging_product: 'whatsapp',
-    recipient_type: 'individual',
-    to,
-    context: { message_id: id },
-    type: 'text',
-    text: { body: message },
-  }
+    const url = `${apiUrl}?phone_number_id=${via}`
+    const data = {
+      messaging_product: 'whatsapp',
+      recipient_type: 'individual',
+      to,
+      context: { message_id: id },
+      type: 'text',
+      text: { body: message },
+    }
 
-  const options = {
-    method: 'POST',
-    headers: {
+    const headers = {
       'Content-Type': 'application/json',
       'X-Api-Key': apiKey,
-    },
-    body: JSON.stringify(data),
-  }
+    }
 
-  fetch(url, options).catch((error) => console.error('Error', error))
+    const response = await axios.post(url, data, { headers })
+
+    if (response.status === 200) {
+      console.log('Message sent successfully')
+      return response.data // Returning response data directly
+    } else {
+      console.error(`Failed to send message. Status: ${response.status}`)
+      return null
+    }
+  } catch (error) {
+    console.error('Error:', error)
+    return null
+  }
 }
